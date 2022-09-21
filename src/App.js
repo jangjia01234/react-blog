17a8825ad1f7bdcb096ab557d305179b9644730d
Jia <71865277+jangjia01234@users.noreply.github.com>
2022-09-22 00:02:45 +0900

    update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      dfdf
-    </div>
-  );
+function App () {
+	return (
+    	<div className="App">
+        	<div className="black-nav">
+            	<div>개발 Blog</div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
