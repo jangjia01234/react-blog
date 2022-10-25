93844beaaea0f945160cae7f594a5f3a27a63118
Jia <71865277+jangjia01234@users.noreply.github.com>
2022-10-26 01:10:24 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// 미리보기 창 띄우기는 npm start
+// JSX 문법 1. class 넣을 땐 className
+// JSX 문법 2. 변수 넣을 땐 { }
+
 function App () {
 	return (
     	<div className="App">
