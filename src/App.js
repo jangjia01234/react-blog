1f6757d8f8ccbb5f5516553dd50e8629787aa6e0
Jia <71865277+jangjia01234@users.noreply.github.com>
2022-10-26 01:41:26 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,9 +20,7 @@ import './App.css';
 
 function App () {
 
-  let[isTitleOne, a] = useState('남자 코트 추천');
-  let[isTitleTwo, b] = useState('해커톤 후기');
-  let[isTitleThree, c] = useState('리액트 꿀팁');
+  let[isTitle, b] = useState(['남자 코트 추천', '카카오 해커톤 후기', '리액트 초보 꿀팁']);
   
 
   return (
@@ -31,15 +29,15 @@ function App () {
         <h4>개발 blog</h4>
       </div>
       <div className="list">
-        <h4>{ isTitleOne }</h4>
+        <h4>{ isTitle[0] }</h4>
         <p>2월 17일 발행</p>
       </div>
 	  <div className="list">
-        <h4>{ isTitleTwo }</h4>
+        <h4>{ isTitle[1] }</h4>
         <p>2월 17일 발행</p>
       </div>
 	  <div className="list">
-        <h4>{ isTitleThree }</h4>
+        <h4>{ isTitle[2] }</h4>
         <p>2월 17일 발행</p>
       </div>
     </div>
