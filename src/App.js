03dce478ded82acd6428ddcef30e1d3c33ed204d
Jia <71865277+jangjia01234@users.noreply.github.com>
2022-10-26 01:25:34 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 // 미리보기 창 띄우기는 npm start
+
 // JSX 문법 1. class 넣을 땐 className
 // JSX 문법 2. 변수 넣을 땐 { }
+// JSX 문법 3. style 넣을 땐 style={{스타일명: '값'}} *camelCase로 작성
+
+// return() 안에는 병렬로 태그 2개 이상 기입 X
+
+// 중요한 데이터는 변수 말고 state에 담기
+// 사용 순서: 
+//    1. import {useState} 2. useState(보관할 자료) 3. let [작명, 작명]
+//    ->   let [글제목, b] = useState('여자 코트 추천');
+
 
 function App () {
-	return (
-    	<div className="App">
-        	<div className="black-nav">
-            	<div>개발 Blog</div>
-            </div>
-        </div>
-    );
+
+  let post = '강남 우동 맛집';
+  let[a, b] = useState('남자 코트 추천');
+  
+
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <h4>개발 blog</h4>
+      </div>
+      <div className="list">
+        <h4>{ post }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+    </div>
+  )
 }
 
 export default App;
